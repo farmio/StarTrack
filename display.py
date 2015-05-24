f0b78d454e61a18f865884c1607ef0a4d131f86b
farmio <farmio@alphart.net>
2015-05-24 17:11:04 +0200

    __init__ display from main

--- display.py
+++ display.py
@@ -5,17 +5,31 @@ from config import lcd
 
 class Display(AdaLCD.Adafruit_CharLCD):
     #spawning two threads updating the display simultaniously couses errors
-        
-    def init(self):
+    def __init__(self, sensor_source):
+        lcd_prefs = [lcd['rs'],
+                     lcd['en'],
+                     lcd['d4'],
+                     lcd['d5'],
+                     lcd['d6'],
+                     lcd['d7'],
+                     lcd['columns'],
+                     lcd['rows']]
+        AdaLCD.Adafruit_CharLCD.__init__(self, *lcd_prefs)
+        self.sensor_source = sensor_source
+        self.init_lcd()
+
+    def init_lcd(self):
         self.clear()
         self.set_cursor(0, 0)
         self.message('S:')
 
-    def rotation_update(self, rot_count):           #display 2:7, 0
+    def rotation_update(self):           #display 2:7, 0
+        rot_count = self.sensor_source.rotation_count
         self.set_cursor(2, 0)
         self.message(str(rot_count).rjust(5))
 
-    def sensor_update(self, sensors, direction):    #display 9:10, 0
+    def sensor_update(self):    #display 9:10, 0
+        sensors = self.sensor_source.sensor_buffer
         self.set_cursor(9, 0)
         sensor_message = ''
         for i in sensors:
@@ -23,16 +37,6 @@ class Display(AdaLCD.Adafruit_CharLCD):
         print 'sensor_message: ', sensor_message
         self.message(str(sensor_message).ljust(2))
 
-
-display = Display(lcd['rs'],
-                  lcd['en'],
-                  lcd['d4'],
-                  lcd['d5'],
-                  lcd['d6'],
-                  lcd['d7'],
-                  lcd['columns'],
-                  lcd['rows']
-                  )
 
 #display segmentation
 #0##3##6##9##2##5##8##
--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from position import Sensor
 from distance import hose
 from delegate import Delegate
 from delegate import Queue
-from display import display
+from display import Display
 
 
-display.init()
+display = Display(Sensor)
 
 #set delegates
 Sensor.rotation_callback = Delegate(Sensor.rotation_callback)
@@ -19,21 +19,15 @@ Sensor.sensor_callback = Delegate(Sensor.sensor_callback)
 #initialise threads
 display_thread = Lock()
 
-def sensor_update():
-    display.sensor_update(Sensor.sensor_buffer, Sensor.direction_buffer)
-
-def rotation_update():
-    display.rotation_update(Sensor.rotation_count)
-
 @Sensor.rotation_callback.callback
 def display_rotation(*args, **kwargs):
-    Queue(rotation_update,
+    Queue(display.rotation_update,
           display_thread,
           rest=0.01).start()
 
 @Sensor.sensor_callback.callback
 def display_sensor(*args, **kwargs):
-    Queue(sensor_update,
+    Queue(display.sensor_update,
           display_thread,
           rest=0.01).start()
 
