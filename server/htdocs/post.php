d2e5be4a34953c9059ba3375d9d5fdf38a41eba4
farmio <farmio@alphart.net>
2016-03-22 10:24:04 +0100

    http codes

--- server/htdocs/post.php
+++ server/htdocs/post.php
@@ -9,9 +9,11 @@ $logger = new Katzgrau\KLogger\Logger('../log/');
 if( $_POST['jwt'] ){
   try {
     $decoded = JWT::decode($_POST['jwt'], $key, array('HS256'));
-    $logger->info($_SERVER['REMOTE_ADDR'] . " - Temp: " . $decoded->temp . " Pos: " . $decoded->pos . " Spd: " . $decoded->spd);
+    http_response_code(202);
+    // $logger->info($_SERVER['REMOTE_ADDR'] . " - Temp: " . $decoded->temp . " Pos: " . $decoded->pos . " Spd: " . $decoded->spd);
   } catch (Exception $e) {
     $logger->error("Token Error - " . $e->getMessage());
+    http_response_code(401);
   }
   try {
     $dbh = new SQLite3('../data/node_data.sqlite');
@@ -28,11 +30,15 @@ if( $_POST['jwt'] ){
     $query = "INSERT INTO node_{$decoded->nid} (Speed, Position, Temperature) VALUES
       ({$decoded->spd}, '{$esc_string}', {$decoded->temp})";
     $dbh->exec($query);
+    http_response_code(201);
   } catch (Exception $e) {
     $logger->error("DB Error - " . $dbh->lastErrorMsg());
+    http_response_code(500);
   } finally {
     $dbh->close();
   }
+} else {
+  http_response_code(400);
 }
 
 ?>
